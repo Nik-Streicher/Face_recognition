ddbee2efd338311fd8ffa96bb465cdfeb0f10b52
Nikolay <nikolay.sintsin@educanet.cz>
2021-01-08 11:39:27 +0100

    initial commit

--- main/single_frame_recognition.py
+++ main/single_frame_recognition.py
@@ -7,22 +7,28 @@ from util.MysqlConnector import MysqlConnector, encode
 detector = FaceDetector()
 
 tested_image = Image.open("../images/multi/3.jpg")
+# ../images/invalid/1.jpg -> invalid picture path
 
-tested_image_embedding = detector.resnet(detector.mtcnn(tested_image))
+mtcnn = detector.mtcnn(tested_image)
 
-mysql = MysqlConnector(host="localhost", user="python_user", password="password", database="python_project")
+if mtcnn is not None:
+    tested_image_embedding = detector.resnet(mtcnn)
 
-database_users = mysql.select_all_users()
+    mysql = MysqlConnector(host="localhost", user="python_user", password="password", database="python_project")
 
-users = encode(database_users=database_users)
+    database_users = mysql.select_all_users()
 
-recognized_users = recognize_users(face_embedding=tested_image_embedding, users=users, distance=0.9)
+    users = encode(database_users=database_users)
 
-boxes, _ = detector.mtcnn.detect(tested_image)
+    recognized_users = recognize_users(face_embedding=tested_image_embedding, users=users, distance=0.9)
 
-image = draw_bounding_box(boxes=boxes, recognized_users=recognized_users, pil_image=tested_image,
-                          font_path='arial.ttf',
-                          font_size=17)
+    boxes, _ = detector.mtcnn.detect(tested_image)
 
-cv2.imshow("Recognized Image", convert_to_cv(pil_image=image))
+    tested_image = draw_bounding_box(boxes=boxes, recognized_users=recognized_users, pil_image=tested_image,
+                                     font_path='arial.ttf',
+                                     font_size=17)
+else:
+    print("no face was detected")
+
+cv2.imshow("Recognized Image", convert_to_cv(pil_image=tested_image))
 cv2.waitKey(0)
