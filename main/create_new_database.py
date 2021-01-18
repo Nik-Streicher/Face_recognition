30c3757e89f33f959adc58fdb3e9fd179b616c0d
Nikolay <nikolay.sintsin@educanet.cz>
2021-01-18 22:20:17 +0100

    initial commit

--- main/create_new_database.py
+++ main/create_new_database.py
@@ -3,9 +3,10 @@ from os import path
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from util import MysqlConnector
+from util import MysqlConnector, return_data_from_the_file
 
-connector = MysqlConnector(host="localhost", user="python_user", password="password")
+host, user, password, _ = return_data_from_the_file("../database_data.txt")
+connector = MysqlConnector(host=host, user=user, password=password)
 
 connector.create_new_database(database_name=input("enter database name: "))
 # database name -> "test_database"
